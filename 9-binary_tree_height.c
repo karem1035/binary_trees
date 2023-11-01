e5c05540d70bb5a24d8ea2400d3c8811d18ddc28
Karem <karemdarwesh1035@gmail.com>
2023-11-01 22:35:32 +0200

    final touches to task 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -12,14 +12,14 @@ int max(int a, int b)
 /**
  * binary_tree_height - gets the hight of a binary tree
  * @tree: the tree to get it's height
- * Return: the height of the tree or 0 otherwise
+ * Return: the height of the tree
 */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left_subtree_height, right_subtree_height;
 
 	if (tree == NULL)
-		return (0);
+		return (-1);
 	left_subtree_height = binary_tree_height(tree->left);
 	right_subtree_height = binary_tree_height(tree->right);
 	return (1 + max(left_subtree_height, right_subtree_height));

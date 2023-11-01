b7cdc2dd0723caeb8c1ef58614aee2c88b315557
Karem <karemdarwesh1035@gmail.com>
2023-11-01 15:54:24 +0200

    checks if a node is root or not

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -6,7 +6,7 @@
  */
 int binary_tree_is_root(const binary_tree_t *node)
 {
-	if (node->parent == NULL && node)
-		return (1);
-	return (0);
+	if (!node || node->parent)
+		return (0);
+	return (1);
 }

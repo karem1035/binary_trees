68480f93f8b960bfc0a6f94ab7be01b2ffe06e28
Karem <karemdarwesh1035@gmail.com>
2023-11-01 03:34:20 +0200

    a documentation to the binary_tree_node function

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,7 +1,10 @@
 #include "binary_trees.h"
 
 /**
- *
+ * binary_tree_node - create a node
+ * @parent: the parend pointer to the new node
+ * @value: the value of the new node
+ * Return: a pointer to the new node
  */
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)

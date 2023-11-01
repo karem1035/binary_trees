95ad10745ff524ac90071f8579357f8c137b101e
Karem <karemdarwesh1035@gmail.com>
2023-11-01 17:45:02 +0200

    checks if a node is root or not

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -5,8 +5,7 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 	if (tree == NULL)
 		return;
 
-	
-		binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->left, func);
 
-		binary_tree_preorder(tree->right, func);
+	binary_tree_preorder(tree->right, func);
 }

8a1004ddb27751e881c5a11f777f51f996cbbf96
Karem <karemdarwesh1035@gmail.com>
2023-11-02 00:24:05 +0200

    height of a binary tree

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,26 +1,19 @@
 #include "binary_trees.h"
 /**
- * max - get the max between 2 numbers
- * @a: the first number
- * @b: the second number
- * Return: the bigger one
- */
-int max(int a, int b)
-{
-	return ((a > b) ? a : b);
-}
-/**
  * binary_tree_height - gets the hight of a binary tree
  * @tree: the tree to get it's height
  * Return: the height of the tree
-*/
+ */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_subtree_height, right_subtree_height;
+	size_t l = 0, r = 0;
 
-	if (tree == NULL)
+	if (!tree)
 		return (0);
-	left_subtree_height = binary_tree_height(tree->left);
-	right_subtree_height = binary_tree_height(tree->right);
-	return (1 + max(left_subtree_height, right_subtree_height));
+	if (tree->left)
+		l = 1 + binary_tree_height(tree->left);
+	if (tree->right)
+		r = 1 + binary_tree_height(tree->right);
+
+	return ((r > l) ? r : l);
 }

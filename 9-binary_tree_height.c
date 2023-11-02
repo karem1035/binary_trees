fc1238d72f9cbd3864c441dd18d8846a6ad78dbb
Karem <karemdarwesh1035@gmail.com>
2023-11-02 20:27:26 +0200

    height func restore

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -6,13 +6,14 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	if (tree)
-	{
-		size_t l = 0, r = 0;
+	size_t l = 0, r = 0;
 
-		l = tree->left ? 1 + binary_tree_height(tree->left) : 1;
-		r = tree->right ? 1 + binary_tree_height(tree->right) : 1;
-		return ((l > r) ? l : r);
-	}
-	return (0);
+	if (!tree)
+		return (0);
+	if (tree->left)
+		l = 1 + binary_tree_height(tree->left);
+	if (tree->right)
+		r = 1 + binary_tree_height(tree->right);
+
+	return ((r > l) ? r : l);
 }

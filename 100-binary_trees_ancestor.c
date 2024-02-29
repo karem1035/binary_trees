be261468e0b88ef8bc2303c9e3ffbe380b0d50ff
Karem <karemdarwesh1035@gmail.com>
2024-02-29 12:58:02 +0200

    task done

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -12,6 +12,8 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 
 	if (!first || !second)
 		return (NULL);
+	if (!first && !second)
+		return NULL;
 
 	if (first == second)
 		return ((binary_tree_t *)first);

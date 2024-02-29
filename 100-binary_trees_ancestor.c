cdaaeca652311f672732ef2d7a4996be441a01ba
Karem <karemdarwesh1035@gmail.com>
2024-02-29 12:42:09 +0200

    LCA task done

---
+++ 100-binary_trees_ancestor.c
@@ -0,0 +1,28 @@
+#include "binary_trees.h"
+/**
+ * binary_trees_ancestor - finds the LCA lowest common ancestor of 2 nodes.
+ * @first: the first node.
+ * @second: the second node.
+ * Return: a pointer to the lowest common ancestor node, or NULL.
+ */
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+				     const binary_tree_t *second)
+{
+	binary_tree_t *currnet;
+
+	if (!first || !second)
+		return (NULL);
+
+	while (first)
+	{
+		currnet = second->parent;
+		while (currnet)
+		{
+			if (currnet == first)
+				return (currnet);
+			currnet = currnet->parent;
+		}
+		first = first->parent;
+	}
+	return (NULL);
+}

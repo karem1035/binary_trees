f45e1f4bf3b9388353f9dbe5805e54268299c92e
Karem Darwesh <38433851+karem1035@users.noreply.github.com>
2024-02-29 07:41:30 +0200

    Update 12-binary_tree_leaves.c shorten

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -8,15 +8,9 @@
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	size_t leaves = 0;
-
-	if (tree == NULL)
+	if (!tree)
 		return (0);
-
-	if (tree->left == NULL && tree->right == NULL)
+	if (!tree->left && !tree->right)
 		return (1);
-	leaves = leaves + binary_tree_leaves(tree->left);
-	leaves = leaves + binary_tree_leaves(tree->right);
-
-	return (leaves);
+	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
